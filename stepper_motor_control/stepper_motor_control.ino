ec3043be781119b39d672ae4e2b15a563e7fae52
James Totah <jtotah@usfca.edu>
2024-12-31 15:18:38 -0600

    Pushing local code from a recent project

---
+++ stepper_motor_control/stepper_motor_control.ino
@@ -0,0 +1,39 @@
+// Define pins for the stepper driver
+#define STEP_PIN 3 // Connect to PUL+
+#define DIR_PIN 4  // Connect to DIR+
+//#define ENABLE_PIN 7 // Optional, connect to ENABLE+ if your driver supports it
+
+// Stepper motor settings
+int stepsPerRevolution = 25600; // Adjust according to your motor's specs
+int speedDelay = 1;        // Microseconds between steps (adjust for speed)
+int dayLen = 86400;
+
+void setup() {
+  // Set pin modes
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
+}
+
+void loop() {
+  // Rotate clockwise
+  digitalWrite(DIR_PIN, HIGH); // Set direction to clockwise
+  stepMotor();
+  // for (int i = 0; i < stepsPerRevolution; i++) {
+  // }
+  delay(dayLen*1000/(stepsPerRevolution*16)); // Pause for 1 second
+
+  // Rotate counterclockwise
+  // digitalWrite(DIR_PIN, LOW); // Set direction to counterclockwise
+  // for (int i = 0; i < stepsPerRevolution*16; i++) {
+  //   stepMotor();
+  // }
+  // delay(1000); // Pause for 1 second
+}
+
+// Function to take a single step
+void stepMotor() {
+  digitalWrite(STEP_PIN, HIGH);
+  delayMicroseconds(speedDelay); // Wait for step duration
+  digitalWrite(STEP_PIN, LOW);
+  delayMicroseconds(speedDelay); // Wait for step duration
+}
